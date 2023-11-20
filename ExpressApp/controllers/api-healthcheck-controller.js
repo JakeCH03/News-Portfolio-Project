76376b03b8b48ea70d90913f9464e73ebdaccfac
jake <jakehuntlyrobinson@gmail.com>
2023-11-20 14:37:19 +0000

    add /api endpoint

--- ExpressApp/controllers/api-healthcheck-controller.js
+++ ExpressApp/controllers/api-healthcheck-controller.js
@@ -1,3 +1,9 @@
+const endpoints = require("../../endpoints.json");
+
 exports.getApiHealth = (req, res) => {
   res.status(200).send();
+};
+
+exports.getApiEndpoints = (req, res) => {
+  res.status(200).send({ endpoints });
 };
