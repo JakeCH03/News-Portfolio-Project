e4d3d64eaf95e87e9b2972ab8f8b4eb828b86456
jake <jakehuntlyrobinson@gmail.com>
2023-11-22 10:10:17 +0000

    update api-comments-controller.js to be a bit cleaner

--- ExpressApp/controllers/api-comments-controller.js
+++ ExpressApp/controllers/api-comments-controller.js
@@ -4,15 +4,10 @@ const { deleteCommentById } = require("../models/api-comments-model");
 exports.deleteComment = (req, res, next) => {
   const id = req.params.comment_id;
 
-  const commentPromises = [];
-
-  if (id) {
-    commentPromises.push(checkExists("comments", "comment_id", id));
-  }
-
-  commentPromises.push(deleteCommentById(id));
-
-  Promise.all(commentPromises)
+  Promise.all([
+    checkExists("comments", "comment_id", id),
+    deleteCommentById(id),
+  ])
     .then(() => {
       res.status(204).send();
     })
