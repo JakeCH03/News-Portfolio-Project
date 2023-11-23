f384956e8dbbcd35965ca598dc4df6e54be8a3f8
jake <jakehuntlyrobinson@gmail.com>
2023-11-23 13:43:36 +0000

    add everything necessary for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,20 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE not set");
 }
 
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 const db = new Pool();
 module.exports = db;
--- start-app.js
+++ start-app.js
@@ -1,3 +1,5 @@
 const app = require("./ExpressApp/app");
 
-app.listen(9090, () => console.log("Listening on port 9090"));
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
